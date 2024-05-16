3a5186d3bba235912c33d9bae68d8ef61aa8fcba
mohamed-groun <grounmed@gmail.com>
2024-05-16 11:35:25 +0200

    try catch and put error in logger

--- src/Services/NewsApiService.php
+++ src/Services/NewsApiService.php
@@ -3,34 +3,42 @@
 namespace App\Services;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
+use Psr\Log\LoggerInterface;
 
 class NewsApiService
 {
     private $client;
     private $apiKey;
+    private $logger;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, LoggerInterface $logger)
     {
         $this->client = $client;
         $this->apiKey = $_ENV['API_KEY_NEWSAPI'];
+        $this->logger = $logger;
     }
-
 
     public function getImages(): array
     {
-
-        $response = $this->client->request(
-            'GET',
-            'https://newsapi.org/v2/top-headlines',
-            [
-                'query' => [
-                    'country' => 'us',
-                    'apikey' => $this->apiKey,
+        try {
+            $response = $this->client->request(
+                'GET',
+                'https://newsapi.org/v2/top-headlinesd',
+                [
+                    'query' => [
+                        'country' => 'us',
+                        'apikey' => $this->apiKey,
+                    ]
                 ]
-            ]
-        );
+            );
 
-        return $response->toArray()["articles"];
+            return $response->toArray()["articles"];
+        } catch (\Exception $e) {
+            $this->logger->error('Failed to fetch images from News API', [
+                'exception' => $e,
+            ]);
+
+            return [];
+        }
     }
 }
--- src/Services/RssFluxService.php
+++ src/Services/RssFluxService.php
@@ -4,29 +4,35 @@ namespace App\Services;
 
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-
+use Psr\Log\LoggerInterface;
 
 class RssFluxService
 {
     private $client;
+    private $logger;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, LoggerInterface $logger)
     {
         $this->client = $client;
-
+        $this->logger = $logger;
     }
+
     public function getFlux(): array
     {
+        try {
+            $response = $this->client->request(
+                'GET',
+                'http://www.commitstrip.com/en/feed/'
+            );
 
-        $response = $this->client->request(
-            'GET',
-            'http://www.commitstrip.com/en/feed/'
-        );
-
-        $xmlEncoder = new XmlEncoder();
+            $xmlEncoder = new XmlEncoder();
             $array = $xmlEncoder->decode($response->getContent(), 'xml');
 
-        return $array["channel"]["item"];
-    }
+            return $array["channel"]["item"];
+        } catch (\Exception $e) {
+            $this->logger->error('An error occurred while fetching RSS feed: ' . $e->getMessage());
 
+            return [];
+        }
+    }
 }
